51430e05384f1fd4586ec6d3ecc93948d2159353
Leila Sadr <leilasadr54@gmail.com>
2023-06-13 15:08:19 -0700

    We created a GET route for the user's profile info on the server side for now

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -2,7 +2,19 @@ import React from "react";
 
 function Overview() {
  return (
-    <h2>Upcoming Gigs</h2>
+    <div className= 'overview'>
+        <h1>Overview</h1>
+        <h2>Upcoming Gigs</h2>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h2>Completed Gigs</h2>
+    </div>
+
     )
 }
 
