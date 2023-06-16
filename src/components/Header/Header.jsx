562e212622277a24453a5543e7c13f7eeaab1e02
Max Simmons <maxsimmons20@gmail.com>
2023-06-16 17:27:25 -0500

    added logo to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
+import './Header.css'
 
 function Header() {
 
     return(
-        <div>
-            <h2>CoachLink</h2>
+        <div className='header'>
+            <img className='logo' src='/images/logo.png' alt='CoackLinklogo'/>
+            <h2 className='h2Text'>CoachLink</h2>
+            
+
         </div>
     )
 }
