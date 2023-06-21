32cc0f12e2ce7f9c749feb49e65ff688c855aa6e
mandi andrade <mandi.andrade20@gmail.com>
2023-06-21 16:36:30 -0500

    swapped out filter toggle for buttons

--- src/components/CoachingJobList/CoachingJobList.jsx
+++ src/components/CoachingJobList/CoachingJobList.jsx
@@ -1,29 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CoachingJobCard from '../CoachingJobCard/CoachingJobCard';
+import FilterButton from '../FilterButton/FilterButton';
 import { FormGroup, FormControlLabel, Switch } from '@mui/material';
 import "./CoachingCardList.css"
+
+// create an object to map the filter options: 
+const FILTER_MAP = {
+	All: () => true,
+	Ski: (gig) => gig.ski_or_snow === "Ski",
+	Snowboard: (gig) => gig.ski_or_snow === "Snowboard"
+}
+
+// use Object.keys() method to collect an array of gig objects:
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function CoachingJobList() {
 	const dispatch = useDispatch();
 	const gigs = useSelector((store) => store.gigs);
-	console.log(gigs);
-	const [filters, setFilters] = useState({
-		skiFilter: false,
-		snowboardFilter: false,
-		uncertifiedFilter: false
-	})
+	// console.log(gigs);
+
+	const [filter, setFilter] = useState("All");
+	// need to filter thru the list of filter options/names:
+	const filterList = FILTER_NAMES.map((name) => (
+		<FilterButton
+			key={name}
+			name={name}
+			isPressed={name === filter}
+			setFilter={setFilter}
+		/>
+	));
+
+	// const [filters, setFilters] = useState({
+	// 	skiFilter: false,
+	// 	snowboardFilter: false
+	// })
 
 	// need a function to handle the toggle:
 	// using spread operator so state isn't mutated and can update one key/value at a time
-	const handleToggle = (filterName) => {
-		const newFilters = {
-			...filters,
-			[filterName]: !filters[filterName]
-		}
-		setFilters(newFilters);
-		console.log('Current state:', newFilters);
-	}
+	// const handleToggle = (filterName) => {
+	// 	const newFilters = {
+	// 		...filters,
+	// 		[filterName]: !filters[filterName]
+	// 	}
+	// 	setFilters(newFilters);
+	// 	console.log('Current state:', newFilters);
+	// }
 
 	//this starts the dispatch to begin our flow - this will reload our side effect whenever the dispatch is changed
 	useEffect(() => {
@@ -40,7 +62,8 @@ function CoachingJobList() {
 		<>
 			<h1>Avaliable Gigs</h1>
 			<h2>Search Filters:</h2>
-			<FormGroup>
+			{filterList}
+			{/* <FormGroup>
 				<FormControlLabel
 					control={
 						<Switch
@@ -68,7 +91,7 @@ function CoachingJobList() {
 					}
 					label="Uncertified"
 				/>
-			</FormGroup>
+			</FormGroup> */}
 
 			<div className='cardContainer'>
 				{gigs.map((gig, index) => {
---
+++ src/components/FilterButton/FilterButton.jsx
@@ -0,0 +1,12 @@
+ export default function FilterButton(props) {
+  return (
+    <button
+      type="button"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}>
+      <span className="visually-hidden">Show </span>
+      <span>{props.name}</span>
+      <span className="visually-hidden"> gigs</span>
+    </button>
+  )
+}
