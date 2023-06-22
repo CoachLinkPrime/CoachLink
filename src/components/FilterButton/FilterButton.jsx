1bfc64388634707267dc8f690a3158c7a36eb8cf
Leila Sadr <leilasadr54@gmail.com>
2023-06-21 17:30:05 -0700

    Made the buttons on the filter feature look consistent with the other buttons with MUI

--- src/components/FilterButton/FilterButton.jsx
+++ src/components/FilterButton/FilterButton.jsx
@@ -1,14 +1,24 @@
  import './FilterButton.css'
+ import Button from '@mui/material/Button';
+
  
  export default function FilterButton(props) {
   return (
-    <button
+    <Button
+    sx={{
+      backgroundColor: '#7EBBF1',
+      '&:hover': {
+        backgroundColor: '#C6E5F3',
+        color: 'black'
+      },
+      color: 'white'
+    }}
       type="button"
       aria-pressed={props.isPressed}
       onClick={() => props.setFilter(props.name)}>
       <span className="visually-hidden">Show </span>
       <span>{props.name}</span>
       <span className="visually-hidden"> gigs</span>
-    </button>
+    </Button>
   )
 }
